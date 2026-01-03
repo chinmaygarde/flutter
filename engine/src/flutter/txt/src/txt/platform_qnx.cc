30774c37caf48b1fcf5e1b9834e1ec5692fe9566
Chinmay Garde <chinmaygarde@gmail.com>
2026-01-03 13:31:02 -0800

    Support font resolution on QNX.

---
+++ engine/src/flutter/txt/src/txt/platform_qnx.cc
@@ -0,0 +1,21 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "txt/platform.h"
+
+#include "third_party/skia/include/ports/SkFontMgr_empty.h"
+
+namespace txt {
+
+std::vector<std::string> GetDefaultFontFamilies() {
+  return {"Ubuntu",      "Adwaita Sans",    "Cantarell",
+          "DejaVu Sans", "Liberation Sans", "Arial"};
+}
+
+sk_sp<SkFontMgr> GetDefaultFontManager(uint32_t font_initialization_data) {
+  static sk_sp<SkFontMgr> mgr = SkFontMgr_New_Custom_Empty();
+  return mgr;
+}
+
+}  // namespace txt
